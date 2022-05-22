d69eed5c4d3b24ee3608e53766ce6638f80ad873
Sagan <61011062@kmitl.ac.th>
2022-05-22 14:48:54 +0700

    make it better

--- website/views.py
+++ website/views.py
@@ -148,7 +148,8 @@ def ytanalyze():
     comment = flat_list[3::4]
 
 
-    comment = data['Comment'].apply(remove3ConsecutiveDuplicates)
+    comment = data['Comment']
+    #.apply(remove3ConsecutiveDuplicates)
     data.Comment=comment
     data.to_csv('youtubecomments.csv', index=False)
 
